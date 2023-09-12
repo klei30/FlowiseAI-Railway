684f1207702bddb360ea796fd39148bfff309b5e
klei aliaj <121291603+klei30@users.noreply.github.com>
2023-09-12 11:49:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-// Serve static files
-app.use(express.static(path.join(__dirname)));
-
-// Set up a route to redirect all requests for the root to the chatbot URL
-app.get('/', (req, res) => {
-  res.redirect('/chatbot/e9794f69-43bd-4598-b346-49bffb1f4aee');
+// Redirect all traffic to the chatbot URL
+app.get('*', (req, res) => {
+  res.redirect('https://flowiseai-railway-production-7b22.up.railway.app/chatbot/e9794f69-43bd-4598-b346-49bffb1f4aee');
 });
 
-// Start the server
 app.listen(80, () => {
   console.log('Server is running on port 80');
 });
